0a869eb2f166aa98eb98d9ae950342098af0c435
Chico <andres.chico@solera.com>
2022-11-14 11:06:05 +0100

    Created the SpringBoot project

---
+++ telegram-bot-service/src/main/java/com/telegrambot/service/telegrambotservice/TelegramBotServiceApplication.java
@@ -0,0 +1,13 @@
+package com.telegrambot.service.telegrambotservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotServiceApplication.class, args);
+	}
+
+}
---
+++ telegram-bot-service/src/test/java/com/telegrambot/service/telegrambotservice/TelegramBotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegrambot.service.telegrambotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
